3f573ec00f1742cd009082dfcb43880fbe2419f3
PiotrK <piotrek380@gmail.com>
2020-12-02 00:10:29 +0100

    App.js - convert main App component to a class component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import { Container } from "@material-ui/core";
-
+import React from "react";
 import ProductList from "./ProductList";
 
-function App() {
-  const products = [
-    {
-      id: 1,
-      title: "Item1",
-    },
-    {
-      id: 2,
-      title: "Item2",
-    },
-    {
-      id: 3,
-      title: "Item3",
-    },
-  ];
+class App extends React.Component {
+  render() {
+    const products = [
+      {
+        id: 1,
+        title: "Item1",
+      },
+      {
+        id: 2,
+        title: "Item2",
+      },
+      {
+        id: 3,
+        title: "Item3",
+      },
+    ];
 
-  return (
-    <main>
-      <Container>
-        <h1>Popular Products</h1>
-        <ProductList products={products} />
-      </Container>
-    </main>
-  );
+    return (
+      <main>
+        <Container>
+          <h1>Popular Products</h1>
+          <ProductList products={products} />
+        </Container>
+      </main>
+    );
+  }
 }
+// function App() {
+//   const products = [
+//     {
+//       id: 1,
+//       title: "Item1",
+//     },
+//     {
+//       id: 2,
+//       title: "Item2",
+//     },
+//     {
+//       id: 3,
+//       title: "Item3",
+//     },
+//   ];
+
+//   return (
+//     <main>
+//       <Container>
+//         <h1>Popular Products</h1>
+//         <ProductList products={products} />
+//       </Container>
+//     </main>
+//   );
+// }
 
 export default App;

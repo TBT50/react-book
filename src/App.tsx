c6dd083114388027ece29fbce52e28b9a03d43e6
Piotrek <piotrek380@gmail.com>
2021-04-19 21:20:05 +0200

    Add a new list with some items

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>VERCEL</h1>
+      </header>
+      <ul>
+        <li>Ferrari</li>
+        <li>Mercedes</li>
+        <li>Mclaren</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;

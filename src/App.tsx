a415d0b5d30e426261acc0712335754e5ef05d0f
Piotrek <piotrek380@gmail.com>
2021-04-21 18:52:19 +0200

    Add 'styled-components' and add AppContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from "react";
 import ListItem from "./components/ListItem";
+import { AppContainer } from "./styles";
+import "./App.css";
+
+const styles: React.CSSProperties = {
+  color: "red",
+  backgroundColor: "blue",
+};
+
+const cars = [
+  {
+    id: 1,
+    carName: "Ferrari",
+  },
+  {
+    id: 2,
+    carName: "Mercedes",
+  },
+  {
+    id: 3,
+    carName: "McLaren",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppContainer>
+      <header>
         <h1>VERCEL</h1>
       </header>
-      <ul>
-        <ListItem car="Ferrari" />
-        <ListItem car="Mercedes" />
-        <ListItem car="Renault" />
-        <ListItem car="RedBull" />
-        <ListItem car="Mclaren" />
-        <ListItem car="AlphaTauri" />
-      </ul>
-      <main>This is the main content</main>
-      <footer>This is the footer</footer>
-    </div>
+      <main>
+        <ul>
+          {cars.map((car) => (
+            <ListItem key={car.id} car={car.carName} />
+          ))}
+        </ul>
+      </main>
+    </AppContainer>
   );
 }
 

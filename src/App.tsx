7d17eb1d6daac018694c1e1afab20851021d5521
Piotrek <piotrek380@gmail.com>
2021-04-21 18:56:36 +0200

    Add 'ColumnContainer and delete 'ListItem'

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
-import ListItem from "./components/ListItem";
-import { AppContainer } from "./styles";
+import { AppContainer, ColumnContainer } from "./styles";
 import "./App.css";
-
-const styles: React.CSSProperties = {
-  color: "red",
-  backgroundColor: "blue",
-};
-
-const cars = [
-  {
-    id: 1,
-    carName: "Ferrari",
-  },
-  {
-    id: 2,
-    carName: "Mercedes",
-  },
-  {
-    id: 3,
-    carName: "McLaren",
-  },
-];
 
 function App() {
   return (
     <AppContainer>
-      <header>
-        <h1>VERCEL</h1>
-      </header>
-      <main>
-        <ul>
-          {cars.map((car) => (
-            <ListItem key={car.id} car={car.carName} />
-          ))}
-        </ul>
-      </main>
+      <ColumnContainer>123</ColumnContainer>
+      <ColumnContainer>123</ColumnContainer>
+      <ColumnContainer>123</ColumnContainer>
     </AppContainer>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 300px 300px 300px;
+  grid-gap: 20px;
   background-color: #3179ba;
   padding: 20px;
+`
+
+export const ColumnContainer = styled.div`
+  background-color: #ebecf0;
+  width: 300px;
+  min-height: 40px;
+  padding: 8px;
 `
